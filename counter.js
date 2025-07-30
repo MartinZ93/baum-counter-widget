83cbcddb13f17b56a6bcf63e5882401a3710d082
Martn <mzink@gp-greenpath.com>
2025-07-30 14:26:52 +0200

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,27 +1,32 @@
 const targetNumber = 3450;
 const flipContainer = document.getElementById('flip-counter');
 
-function renderCounter(number) {
-  flipContainer.innerHTML = ''; // Clear existing digits
+function createDigitElement(digit) {
+  const digitEl = document.createElement('div');
+  digitEl.className = 'digit';
+
+  const span = document.createElement('span');
+  span.textContent = digit;
+
+  digitEl.appendChild(span);
+  return digitEl;
+}
+
+function animateCounter(number) {
   const digits = number.toString().padStart(4, '0').split('');
+  flipContainer.innerHTML = '';
   digits.forEach((digit, i) => {
-    const digitEl = document.createElement('div');
-    digitEl.className = 'digit';
-
-    const span = document.createElement('span');
-    span.textContent = digit;
-
-    digitEl.appendChild(span);
+    const digitEl = createDigitElement(digit);
     flipContainer.appendChild(digitEl);
   });
 }
 
 let current = 0;
 const interval = setInterval(() => {
-  current += Math.ceil((targetNumber - current) / 10);
-  renderCounter(current);
-  if (current >= targetNumber) {
-    renderCounter(targetNumber);
+  if (current <= targetNumber) {
+    animateCounter(current);
+    current++;
+  } else {
     clearInterval(interval);
   }
-}, 100);
+}, 30);
